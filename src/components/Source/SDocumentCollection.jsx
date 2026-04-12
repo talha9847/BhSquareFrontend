12e1f9ef003e94eddda556a3af6594b70a6163a1
talha9847 <cse.210840131054@gmail.com>
2026-04-12 23:38:46 +0530

    added

--- src/components/Source/SDocumentCollection.jsx
+++ src/components/Source/SDocumentCollection.jsx
@@ -36,10 +36,15 @@ const SDocumentCollection = () => {
   const [newDocName, setNewDocName] = useState("");
   const [leadsData, setLeadsData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+
   const apiUrl = import.meta.env.VITE_API_URL;
 
   const [docId, setDocId] = useState(0);
   const [gotoNext, setGotoNext] = useState(false);
+  const [regLoading, setRegLoading] = useState(false);
+  const [isFound, setIsFound] = useState(false);
+  const [docInfo, setDocInfo] = useState({});
 
   const [documents, setDocuments] = useState([
     { id: 1, name: "Aadhar Card", file: null },
@@ -51,7 +56,35 @@ const SDocumentCollection = () => {
 
   // --- NEW: Validation State ---
 
+  const saveRegistratin = async () => {
+    if (!validate("registration")) {
+      toast.error("Please fill all registration fields");
+      return;
+    }
+    try {
+      setRegLoading(true);
+      const res = await axios.put(`/api/docs/upsertCustomerDocument`, docInfo, {
+        withCredentials: true,
+      });
+      if (res.status == 200) {
+        getDocInfo();
+        setRegLoading(false);
+        setIsFound(true);
+        toast.success(
+          isFound ? "Updated Successfully" : "Created Successfully",
+        );
+      }
+    } catch (error) {
+      setRegLoading(false);
+      toast.error("Error saving registration");
+    }
+  };
+
   const saveAllDocs = async () => {
+    if (!isFound) {
+      toast.error("Fill detail above and then upload docs");
+      return;
+    }
     const hasFiles = documents.some((doc) => doc.file !== null);
     if (!hasFiles) {
       toast.error("Please select at least one document to upload");
@@ -90,7 +123,33 @@ const SDocumentCollection = () => {
       setDocLoad(false);
     }
   };
+  const validate = (type) => {
+    let tempErrors = {};
+    if (type === "identity") {
+      if (!leadsData.customer_name?.trim())
+        tempErrors.customer_name = "Full name is required";
+      if (!leadsData.contact_number?.trim())
+        tempErrors.contact_number = "Contact is required";
+      else if (!/^\d{10}$/.test(leadsData.contact_number))
+        tempErrors.contact_number = "Enter valid 10-digit number";
+      if (!leadsData.address?.trim())
+        tempErrors.address = "Address is required";
+    }
 
+    if (type === "registration") {
+      if (!docInfo.consumer_number?.trim())
+        tempErrors.consumer_number = "Consumer number required";
+      if (!docInfo.geo_coordinate?.trim())
+        tempErrors.geo_coordinate = "Coordinates required";
+      if (!docInfo.sub_division?.trim())
+        tempErrors.sub_division = "Sub division required";
+      // if (!docInfo.registration_number?.trim())
+      //   tempErrors.registration_number = "Registration ID required";
+    }
+
+    setErrors(tempErrors);
+    return Object.keys(tempErrors).length === 0;
+  };
   const confirmAddDoc = () => {
     if (newDocName.trim()) {
       setDocuments((prevDocs) => [
@@ -111,6 +170,36 @@ const SDocumentCollection = () => {
       prevDocs.map((doc) => (doc.id === id ? { ...doc, file: file } : doc)),
     );
   };
+
+  const getDocInfo = async () => {
+    try {
+      const res = await axios.get(
+        `/api/docs/getCustomerDocumentByCustomerId/${customerId}`,
+        { withCredentials: true },
+      );
+      if (res.data.data) {
+        setDocInfo(res.data.data);
+        console.log("i am talking about this");
+        console.log(res.data.data);
+        setDocId(res.data.data.id);
+        setIsFound(true);
+      } else {
+        setDocInfo({
+          customer_id: customerId,
+          consumer_number: "",
+          geo_coordinate: "",
+          registration_number: "",
+          sub_division: "",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getDocInfo();
+  }, []);
 
   return (
     <div className="min-h-screen bg-slate-50 flex font-syne">
@@ -146,6 +235,73 @@ const SDocumentCollection = () => {
 
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             <div className="lg:col-span-2 space-y-6">
+              <div className="bg-white p-8 rounded-[32px] border border-slate-200 shadow-sm">
+                <div className="flex justify-between items-center mb-8">
+                  <h2 className="text-[10px] font-black text-slate-400 uppercase tracking-widest flex items-center gap-2">
+                    <ClipboardCheck size={14} className="text-[#1a5695]" />{" "}
+                    Registration Info
+                  </h2>
+                  <button
+                    onClick={saveRegistratin}
+                    disabled={regLoading}
+                    className="bg-[#1a5695] text-white px-6 py-2.5 rounded-xl font-bold text-[10px] uppercase flex items-center gap-2 hover:bg-[#15467a] transition-all shadow-md"
+                  >
+                    {regLoading ? (
+                      <>
+                        <Save size={14} />{" "}
+                        {isFound ? "Updating..." : "Saving..."}
+                        <Loader2 className="animate-spin h-4 w-4" />
+                      </>
+                    ) : (
+                      <>
+                        <Save size={14} />{" "}
+                        {isFound ? "Update Data" : "Save Data"}
+                      </>
+                    )}
+                  </button>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <CustomInput
+                    label="Consumer Number"
+                    icon={<Hash size={14} />}
+                    value={docInfo.consumer_number}
+                    error={errors.consumer_number}
+                    onChange={(v) => {
+                      setDocInfo({
+                        ...docInfo,
+                        consumer_number: v.toUpperCase(),
+                      });
+                      if (errors.consumer_number)
+                        setErrors({ ...errors, consumer_number: null });
+                    }}
+                  />
+                  <CustomInput
+                    label="Geo Coordinates"
+                    icon={<Globe size={16} />}
+                    value={docInfo.geo_coordinate}
+                    error={errors.geo_coordinate}
+                    onChange={(v) => {
+                      setDocInfo({
+                        ...docInfo,
+                        geo_coordinate: v.toUpperCase(),
+                      });
+                      if (errors.geo_coordinate)
+                        setErrors({ ...errors, geo_coordinate: null });
+                    }}
+                  />
+                  <CustomInput
+                    label="Sub Division"
+                    icon={<Navigation size={16} />}
+                    value={docInfo.sub_division}
+                    error={errors.sub_division}
+                    onChange={(v) => {
+                      setDocInfo({ ...docInfo, sub_division: v.toUpperCase() });
+                      if (errors.sub_division)
+                        setErrors({ ...errors, sub_division: null });
+                    }}
+                  />
+                </div>
+              </div>
               {/* Documentation */}
               <div className="bg-white p-8 rounded-[40px] border border-slate-200 shadow-sm">
                 <div className="flex justify-between items-center mb-8">
